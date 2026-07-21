102bd122ddbb83667c119ab589bfed8e6982c419
AelStGermain <sofia.gomez.orellana@gmail.com>
2026-07-21 15:04:07 -0400

    Fix PORT para Render

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ async function firestoreQuery({ sinceDate = null, pageSize = 2000 } = {}) {
 // ──────────────────────────────────────────────────────────────────────────
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
